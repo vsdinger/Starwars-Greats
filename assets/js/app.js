a79b4d3d9aa9823081152c596504eff95743eec3
unknown <diesol42@gmail.com>
2019-04-27 17:54:06 -0700

    retrying

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,13 @@
+// Linking Recipes API
+var type = $(this).attr("data-type");
+var queryURL = "https://api.edamam.com/search?q=shrimp&r=coconut&app_id=e01c42d8&app_key=19a34826099c7e0c9666127afe12981b"
+console.log(queryURL);
+// still need to get the "r" search term to work
+
 $.ajax({
-    dataType: "json",
-    url: url,
-    data: data,
-    success: success
+    url: queryURL,
+    method: "GET",
   });
+
+// click
+document.getElementById('NAV').click();
