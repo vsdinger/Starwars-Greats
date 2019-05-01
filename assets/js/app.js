0b08f842b0e58a21987e455fe68eeb9e0cbdbe53
unknown <diesol42@gmail.com>
2019-04-30 20:01:07 -0700

    added firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,18 @@
+//Firebase
+  var config = {
+    apiKey: "AIzaSyAQ9LlCCGsxumNoOpvX1BCNHA-zTmEYZ98",
+    authDomain: "recipetorestaurant.firebaseapp.com",
+    databaseURL: "https://recipetorestaurant.firebaseio.com",
+    projectId: "recipetorestaurant",
+    storageBucket: "recipetorestaurant.appspot.com",
+    messagingSenderId: "49045247832"
+  };
+  firebase.initializeApp(config);
+
+
+//shortcut for our firebase - firebase.database() will grab anything in our firebase
+var database = firebase.database();
+
 // Linking Recipes API
 var type = $(this).attr("data-type");
 var queryURL = "https://api.edamam.com/search?q=shrimp&r=coconut&app_id=e01c42d8&app_key=19a34826099c7e0c9666127afe12981b"
@@ -9,5 +24,22 @@ $.ajax({
     method: "GET",
   });
 
-// click
-document.getElementById('NAV').click();
+// grab search fields
+$("#future-button").on("click", function(event) {
+  event.preventDefautl();
+
+  var recipe = $("#recipe-input").val().trim();
+  var restaurant = $("#restaurant-recipe").val().trim();
+  console.log(recipe);
+  console.log(restaurant);
+  
+  //send our data to firebase
+  var searchFirebase = {
+      sRecipe: recipe,
+      sRestaurant:restaurant,
+  };
+})
+
+firebase.database().ref().on("child_added", function(snapshot){
+    $()
+})
